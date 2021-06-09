098224b903b9d9e705193a72501e1d409c044cf7
BlackDiamond SC (BSC Tokens) <73549208+1Blackdiamondsc@users.noreply.github.com>
2021-06-09 18:43:07 -0400

    Add files via upload

---
+++ BSC Bitcoin Node 1.ts
@@ -0,0 +1,13 @@
+curl https://youthful-kowalevski:shelve-muppet-blunt-grew-common-embody@nd-971-729-432.p2pify.com -d '{"method":"getblockchaininfo","params":[],"id":1}'
+from bitcoincli import Bitcoin
+
+host = "nd-971-729-432.p2pify.com"
+port = "80"
+username = "youthful-kowalevski"
+password = "shelve-muppet-blunt-grew-common-embody"
+
+bitcoin = Bitcoin(youthful-kowalevski, shelve-muppet-blunt-grew-common-embody, nd-971-729-432.p2pify.com, 80)
+info = bitcoin.getblockchaininfo(any)
+print(info)
+info = bitcoin.getblockchaininfo(any)
+print(info)
