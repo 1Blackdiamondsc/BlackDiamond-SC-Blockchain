4953582201745a61f21fe2ac297f136d60a4edda
BlackDiamond SC (BSC Tokens) <73549208+1Blackdiamondsc@users.noreply.github.com>
2021-06-09 18:44:00 -0400

    Add files via upload

---
+++ BSC Bitcoin Testnet.ts
@@ -0,0 +1,11 @@
+curl https://stoic-hodgkin:deacon-ascend-legend-jailer-hardly-uptake@ nd-613-011-133.p2pify.com-d '{"method":"getblockchaininfo","params":[],"id":1}'
+from bitcoincli import Bitcoin
+
+host = "nd-613-011-133.p2pify.com"
+port = "80"
+username = "stoic-hodgkin"
+password = "deacon-ascend-legend-jailer-hardly-uptake"
+
+bitcoin = Bitcoinstoic-(stoic-hodgkin, deacon-ascend-legend-jailer-hardly-uptake, nd-613-011-133.p2pify.com, 80)
+info = bitcoin.getblockchaininfo()
+print(info)
